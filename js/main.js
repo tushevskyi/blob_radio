92bed931aa6a130d43f209c1973e743812e8bd1a
Bogdan Tushevskyi <tushevskuy@gmail.com>
2017-05-15 18:49:33 +0300

    change video path

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@ $(function() {
 var info = document.getElementById('info');
 function onYouTubePlayerAPIReady() {
   var player = new YT.Player('player', {
-      videoId: '2f4ztyFMot4', // this is the id of the video at youtube (the stuff after "?v=")
+      videoId: 'xrbrQhpvn8E', // this is the id of the video at youtube (the stuff after "?v=")
       events: {
           onReady: function (e) {
               console.log('video is loaded');
